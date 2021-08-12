d4c27085c3a61c783bbed843d1af521ea47ceed4
scastd00 <scastd00@estudiantes.unileon.es>
2021-08-12 16:09:57 +0200

    Initial commit after initializing app with vue-cli

---
+++ client/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+
+  rules: {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    indent: ['error', 2],
+    semi: ['error', 'always'],
+    quotes: ['warn', 'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    'space-before-function-paren': ['error', 'never'],
+    'space-in-parens': ['error', 'never'],
+    'max-len': ['error', {
+      code: 100,
+      comments: 100
+    }],
+    'multiline-ternary': ['error', 'never'],
+    'eol-last': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', {
+      max: 2,
+      maxEOF: 0
+    }]
+  }
+};
